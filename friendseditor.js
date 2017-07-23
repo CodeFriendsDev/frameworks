23a9ccee6147a361b98d40ed8afc9ddf1874e954
Yoshito Imai <yoshito.imai@gmail.com>
2017-07-23 14:44:01 +0900

    update friendseditor.js

--- friendseditor.js
+++ friendseditor.js
@@ -1,4 +1,4 @@
-function createKey(key) {
+function _createKey(key) {
     var _pref = "";
     if (getUserName) _pref += getUserName() + "_";
     if (getUserSave) _pref += getUserSave() + "_";
@@ -6,13 +6,31 @@ function createKey(key) {
 }
 
 function getLocalStorage(key) {
-    return localStorage.getItem(createKey(key));
+    return localStorage.getItem(_createKey(key));
 }
 
 function setLocalStorage(key, value) {
-    localStorage.setItem(createKey(key), value);
+    localStorage.setItem(_createKey(key), value);
 }
 
 function removeLocalStorage(key) {
-    localStorage.removeItem(createKey(key));
+    localStorage.removeItem(_createKey(key));
+}
+
+function _loadScript(url) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+};
+ 
+function loadScript(id, slot) {
+    var nowTime = new Date();
+    var time = nowTime.getFullYear();
+    time += "0" + (nowTime.getMonth() + 1).toString().slice(-2);
+    time += "0" + nowTime.getDate().toString().slice(-2);
+    time += "0" + nowTime.getHours().toString().slice(-2);
+    time += "0" + nowTime.getMinutes().toString().slice(-2);
+    time += "0" + nowTime.getSeconds().toString().slice(-2);
+    _loadScript("sources/" + id + "/main_" + slot + ".js?" + time);
 }
