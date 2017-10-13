bcfc2b21e9fce76075e98d6d09d7bf92a4f6ce78
Yoshito Imai <yoshito.imai@gmail.com>
2017-10-13 20:18:51 +0900

    Create template

---
+++ template/main.js
@@ -0,0 +1,39 @@
+var assets = [
+    "images/title.png",// タイトル
+];
+
+function gameStart(){
+    var scene = new Scene();
+    core.replaceScene(scene); core.resume();
+
+    //==========
+    // ここから
+    //==========
+    
+    scene.backgroundColor = "red";
+    
+    //==========
+    // ここまで
+    //==========
+
+};
+
+function titleStart(){// タイトル画面
+    var scene = gameManager.createTitleScene();
+    core.replaceScene(scene); core.pause();
+    scene.on(enchant.Event.TOUCH_START, function(){gameStart();});
+}
+
+//==========
+//EnchantJS
+enchant();
+var gameManager;
+var core;
+var scene;
+window.onload = function(){
+    gameManager = new common.GameManager();
+    core = gameManager.createCore(320, 480);
+    core.preload(assets);
+    core.onload = function(){titleStart();};
+    core.start();
+};
