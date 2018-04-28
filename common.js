1bd22c3161244fb791990518e35c07db5a56469f
Yoshito Imai <yoshito.imai@gmail.com>
2018-04-29 01:59:26 +0900

    rot13追加

--- common.js
+++ common.js
@@ -27,3 +27,15 @@ function getRotation(originX, originY, destX, destY) {
     var radian = getRadian(originX, originY, destX, destY);
     return radian / (Math.PI / 180) + 90;
 }
+
+function rot13(str) {
+    return str.split('').map.call(str, function(char) {
+        x = char.charCodeAt(0);
+        if (x < 65 || x > 90) {
+            return String.fromCharCode(x);
+        } else if (x < 78) {
+            return String.fromCharCode(x + 13);
+        }
+        return String.fromCharCode(x - 13);
+    }).join('');
+}
